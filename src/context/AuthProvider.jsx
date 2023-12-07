388af225097ddabb7e17223601151964ae5ea4af
Foysal Ahmmed <ifoysalahmmed@gmail.com>
2023-12-07 22:10:21 +0600

    added AuthProvider component

---
+++ src/context/AuthProvider.jsx
@@ -0,0 +1,77 @@
+import { createContext } from "react";
+import {
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+import app from "../firebase/firebase.config";
+import { useState } from "react";
+import { useEffect } from "react";
+
+export const AuthContext = createContext(null);
+
+const auth = getAuth(app);
+
+const googleProvider = new GoogleAuthProvider();
+
+const AuthProvider = () => {
+  const [user, setUser] = useState(null);
+
+  const [loading, setLoading] = useState(true);
+
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const signIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const updateUserProfile = (name) => {
+    return updateProfile(auth.currentUser, {
+      displayName: name,
+    });
+  };
+
+  const googleSignIn = () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+
+    return () => {
+      return unsubscribe();
+    };
+  }, []);
+
+  const authInfo = {
+    user,
+    loading,
+    createUser,
+    signIn,
+    updateUserProfile,
+    googleSignIn,
+    logOut,
+  };
+
+  return <AuthContext.Provider value={authInfo}></AuthContext.Provider>;
+};
+
+export default AuthProvider;
--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,12 @@ import { RouterProvider } from "react-router-dom";
 import routes from "./routes/routes.jsx";
 import { Provider } from "react-redux";
 import store from "./redux/store.js";
+import AuthProvider from "./context/AuthProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <RouterProvider router={routes} />
-  </Provider>
+  <AuthProvider>
+    <Provider store={store}>
+      <RouterProvider router={routes} />
+    </Provider>
+  </AuthProvider>
 );
