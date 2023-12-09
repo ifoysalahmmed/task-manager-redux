d36dda02cb6219446b2b61d98b57109f2c0d206a
Foysal Ahmmed <ifoysalahmmed@gmail.com>
2023-12-09 20:27:42 +0600

    added success, error message & navigation in SocialLogin

--- src/pages/shared/SocialLogin.jsx
+++ src/pages/shared/SocialLogin.jsx
@@ -1,8 +1,22 @@
-import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { googleLogin } from "../../redux/features/user/userSlice";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
 
 const SocialLogin = () => {
+  const { email } = useSelector((state) => state.userSlice);
+
   const dispatch = useDispatch();
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (email) {
+      toast.success("Logged in successfully");
+      navigate("/");
+    }
+  }, [email, navigate]);
 
   const handleGoogleLogin = () => {
     dispatch(googleLogin());
